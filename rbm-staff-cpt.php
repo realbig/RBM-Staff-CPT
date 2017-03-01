4167693dabf49decf56725277ca637f694435c82
Eric Defore <eric.defore@gmail.com>
2017-03-01 08:48:19 -0500

    v0.1.1

--- rbm-staff-cpt.php
+++ rbm-staff-cpt.php
@@ -3,7 +3,7 @@
 Plugin Name: RBM Staff CPT
 Plugin URL: https://github.com/realbig/RBM-Staff-CPT
 Description: Staff CPT moved from CPT-onomies
-Version: 0.1.0
+Version: 0.1.1
 Text Domain: rbm-staff-cpt
 Author: Eric Defore
 Author URL: http://realbigmarketing.com
